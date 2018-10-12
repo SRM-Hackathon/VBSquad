1dcb3e634bcea75b72061b26cf0b6001d088c31b
Vijayanand <vijayanand.pg@gmail.com>
2018-10-12 16:28:11 +0530

    QA Bot initial commit

---
+++ ICH-QABot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=qaassist-Web-Deploy.pubxml -p:Password=FwBDG6vBYpQjhx5YoScMMM0m9YwGQLFXQQoQJmS0tmxAAvvtdZHyzGWY7yN2
+
