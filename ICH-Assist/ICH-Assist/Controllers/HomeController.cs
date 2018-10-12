0c681c8b238a4f8a33a570d0e052142a12d8eeb1
Vijayanand <vijayanand.pg@gmail.com>
2018-10-12 18:26:54 +0530

    POS analysis alg - draft

--- ICH-Assist/ICH-Assist/Controllers/HomeController.cs
+++ ICH-Assist/ICH-Assist/Controllers/HomeController.cs
@@ -32,27 +32,106 @@ namespace ICH_Assist.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Process(string para)
+        public ActionResult Process(string para,string question)
         {
+
+            string ques_proper_noun = "", ques_noun = "", ques_verb = "";
+            string[] sentences = para.Split('.');
+            string[] occurances = { };
+            //Getting question info
+            ques_verb= GetAllVerbs(question).Replace("/","").Replace("\\","").Replace("VBN","").Replace( "VBG","").Replace( "VBP","").Replace("VBZ","").Replace("VBD","").Replace("VB","");
+            ques_proper_noun= GetAllProperNouns(question);
+            ques_noun = GetAllNouns(question);
+            ViewBag.Message = GetAllVerbs(question);
+            ViewBag.Message = GetAllProperNouns(question);
+            ViewBag.Message = GetAllNouns(question);
+
+
+            int i = 0;
+            if (!ques_proper_noun.Equals(""))
+            {
+               
+                    foreach (string sentenc in sentences)
+                    {
+                        if (sentenc.Contains(ques_proper_noun))
+                            occurances[i++] = sentenc;
+                    }
+               
+                
+            }
+            //Getting para info
+            //ViewBag.Message = GetAllVerbs(occurances[j]);
+            //ViewBag.Message = GetAllProperNouns(occurances[j]);
+            //ViewBag.Message = GetAllNouns(occurances[j]);
+
+            return View();
+        }
+        public  string GetAllVerbs(string para)
+        {
+            
             //api key
             //29809699 - a20d - 448a - 85a8 - 83956f239c00
             Configuration.Default.AddApiKey("Apikey", "29809699-a20d-448a-85a8-83956f239c00");
-            var apiInstance = new PosTaggerStringApi();
+            var apiInstance = new WordsApi();
             var input = para;  // string | Input string
 
-
+            string result = "";
             try
             {
-                // Part-of-speech tag a string
-                string result = apiInstance.PosTaggerStringPost(input);
+                // Get the verbs in a string
+                result = apiInstance.WordsPost(input);
                 Debug.WriteLine(result);
-                ViewBag.Message = result;
+                
             }
             catch (Exception e)
             {
-                Debug.Print("Exception when calling PosTaggerStringApi.PosTaggerStringPost: " + e.Message);
+                Debug.Print("Exception when calling WordsApi.WordsPost: " + e.Message);
             }
-            return View();
+            return result;
+        }
+        public string GetAllProperNouns(string para)
+        {
+
+            
+            Configuration.Default.AddApiKey("Apikey", "29809699-a20d-448a-85a8-83956f239c00");
+            
+            var apiInstance = new WordsApi();
+            var input = para;  // string | Input string
+            string result = "";
+            try
+            {
+                // Get proper nouns in a string
+                result = apiInstance.WordsProperNouns(input);
+                Debug.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Debug.Print("Exception when calling WordsApi.WordsProperNouns: " + e.Message);
+            }
+            
+            return result;
+        }
+        public string GetAllNouns(string para)
+        {
+
+            //api key
+            //29809699 - a20d - 448a - 85a8 - 83956f239c00
+            // Configure API key authorization: Apikey
+            Configuration.Default.AddApiKey("Apikey", "29809699-a20d-448a-85a8-83956f239c00");
+            var apiInstance = new WordsApi();
+            var input = para;  // string | Input string
+            string result = "";
+            try
+            {
+                // Get nouns in string
+                result=apiInstance.WordsNouns(input);
+                Debug.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Debug.Print("Exception when calling WordsApi.WordsNouns: " + e.Message);
+            }
+            return result;
         }
     }
 }
