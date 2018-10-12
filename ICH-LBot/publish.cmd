f63c58df753e6d40a1738bb348102e46f7612860
vijayanand-pg <vijay_006@hotmail.com>
2018-10-12 22:44:13 +0530

    push error

--- ICH-LBot/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=assistbot01-Web-Deploy.pubxml -p:Password=ch0akjEk8dkJ67e7cMoXwmHr5graY1JeBdZfmoNvyLhbo8XXTfg9NYFZyY1T
-
